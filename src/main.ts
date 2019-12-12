d338b45067b9ff8e5f29d18a9cb9212eb8b52888
Joey Kendall <joey-kendall@pluralsight.com>
2019-12-12 16:30:45 -0700

    changing setup a tad to remove logger from fastify

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import { Logger } from 'nestjs-pino'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({logger:true}));
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({}));
   app.useLogger(app.get(Logger))
-  app.setGlobalPrefix('whatever')
+  //app.setGlobalPrefix('whatever')
   await app.listen(3000);
 
 }
